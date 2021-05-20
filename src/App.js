193355f7b779863af6f2704d1d8f2cfd6bb1d521
Aditya Pratap Singh <cybercreed010@gmail.com>
2021-05-20 12:52:34 +0530

    Added Url Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 
 import './App.css';
 import { HomePage } from './pages/homepage'
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+const JacketsPage = () => (
+  <div>
+    <h1>Jackets..</h1>
+  </div>
+);
+
+
 function App() {
   return (
-    <HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route path='/jackets' component={JacketsPage} />
+      </Switch>
+    </div>
   );
 }
 
